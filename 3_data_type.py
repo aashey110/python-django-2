851ba6cdf3fe99dcb0bc1382b732f9eec94d5373
Aashish <aashey110@gmail.com>
2024-08-06 17:40:21 +0545

    python basic exercise

---
+++ 3_data_type.py
@@ -0,0 +1,4 @@
+data = "hello world"
+data_integer = 54
+print(data)
+print(data_integer)
---
+++ 4_type_checking.py
@@ -0,0 +1,5 @@
+data_hello_world_1 = 224
+data_hello_world_2 = '224'
+
+print(type(data_hello_world_1))
+print(type(data_hello_world_2))
