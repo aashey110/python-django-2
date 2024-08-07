86fdfc06ab711e310508a2a51874632f78bec761
Aashish <aashey110@gmail.com>
2024-08-07 17:04:18 +0545

    new files added

--- 5_arithmetic_operator.py
+++ 5_arithmetic_operator.py
@@ -3,18 +3,18 @@ num1 = 10
 num2 = 20
 
 sum = num1 + num2    #ADDITION
-print(sum)  # Output: 30
+print(sum)  
 
 difference = num1 - num2    #SUBTRACTION
-print(difference)  # Output: -10
+print(difference)  
 
 product = num1 * num2   #MULTIPLICATION
-print(product)  # Output: 200
+print(product)  
 
 quotient = num2 / num1      #DIVISION
-print(quotient)  # Output: 2.0
+print(quotient)  
 
 Remainder = num1 / num2     #Moudulus DIVISION
-print(Remainder)  # Output: 0.0
+print(Remainder)  
 
 
---
+++ 9_bitwise_operator.py
@@ -0,0 +1,17 @@
+a = 5   # 00000101 in binary
+b = 9   # 00001001 in binary
+
+print("a =", a, "b =", b)
+
+print("a & b =", a & b)   
+
+print("a | b =", a | b)   
+
+print("a ^ b =", a ^ b)   
+
+print("~a =", ~a)   
+
+print("b << 1 =", b << 1)   
+
+
+print("b >> 1 =", b >> 1)   
