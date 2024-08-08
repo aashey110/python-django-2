107d2bf49539b49aeb10ee46728a944e2b58199b
Aashish <aashey110@gmail.com>
2024-08-08 23:04:22 +0545

    calculator app

---
+++ 14_calculator.py
@@ -0,0 +1,26 @@
+num1 = int(input("Enter first operand: "))
+num2 = int(input("Enter second operand: "))
+
+print("Select one of the following operation to be performed:\n + (addition)\n-(substraction)\n* (multiplication)\n/ (division)")
+
+operator = input("Enter the operator: ")
+
+if operator == "+":
+    sum = num1 + num2
+    print(num1," + ", num2 , " = ", sum)
+
+elif operator == "-":
+    difference = num1 - num2
+    print(num1," - ", num2 , " = ", difference)
+
+elif operator == "*":
+    product = num1 * num2
+    print(num1," * ", num2 , " = ", product)
+
+elif operator == "/":
+    quotient = num1 / num2
+    print(num1," / ", num2 , " = ", quotient)
+
+else:
+    print("Invalid operator, Please enter valid operator")
+    
